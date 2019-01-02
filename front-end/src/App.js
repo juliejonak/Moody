b383c6e4de02f9665ba7b3009790624b2426f6b3
Julie Jonak <juliejonak10@gmail.com>
2019-01-02 14:16:45 -0600

    Created front end components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import {Route} from 'react-router-dom';
+import Form from './components/form';
+import Insights from './components/insights';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount = () => {
+    axios.get('')
+      .then( response => {
+        this.setState({ data: response.data })
+      })
+      .catch( err => console.log(err) )
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Route path='/' render={(props) => <Form {...props} />} />
+        <Route path='/results' reunder={(props) => <Insights {...props} /> } />
       </div>
     );
   }
-}
-
-export default App;
+};
---
+++ front-end/src/components/form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Form extends React.Component{
+    constructor(props){
+
+    };
+
+    
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+};
