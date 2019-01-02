cdef9a18ef4a23c7021955c8f06f43365c6beeda
Julie Jonak <juliejonak10@gmail.com>
2019-01-02 15:10:16 -0600

    Updated form component

--- front-end/src/components/insights.js
+++ front-end/src/components/insights.js
@@ -2,15 +2,14 @@ import React from 'react';
 
 export default class Insights extends React.Component{
     constructor(props){
+        super(props);
 
     };
-
     
 
     render(){
         return(
             <div>
-                
             </div>
         )
     }
