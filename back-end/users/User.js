c8a8fa30f8b999bcc5c841eb9b362d93f15719a7
Justin Kaseman <justinkaseman@live.com>
2019-01-02 16:10:37 -0600

    User Schema

--- back-end/users/User.js
+++ back-end/users/User.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const User = new mongoose.Schema({
   key: { type: Number, required: true, unique: true },
+  name: { type: String },
+  gender: { type: Boolean },
+  age: { type: Number },
+  days_key: { type: Number },
+  days: [{ type: ObjectId, ref: "Day" }],
 });
 
 module.exports = mongoose.model("User", User);
