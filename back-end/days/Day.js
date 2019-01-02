3c0fc02f6f142b40e150e3ec317b8fff1418d57b
Justin Kaseman <justinkaseman@live.com>
2019-01-02 16:25:35 -0600

    Days Schema

--- back-end/days/Day.js
+++ back-end/days/Day.js
@@ -1,7 +1,22 @@
 const mongoose = require("mongoose");
 
+const TAGS = ["Travel", "Work", "Sick", "Date"];
+
 const Day = new mongoose.Schema({
   key: { type: Number, required: true, unique: true },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  happiness: { type: Number },
+  stress: { type: Number },
+  energy: { type: Number },
+  sleep: { type: Number },
+  water: { type: Number },
+  alcohol: { type: Number },
+  caffiene: { type: Number },
+  food: { type: Number },
+  tags: { type: String, enum: TAGS },
 });
 
 module.exports = mongoose.model("Day", Day);
--- back-end/users/User.js
+++ back-end/users/User.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
+const GENDERS = ["M", "F"];
+
 const User = new mongoose.Schema({
   key: { type: Number, required: true, unique: true },
   name: { type: String },
-  gender: { type: Boolean },
+  gender: { type: String, enum: GENDERS },
   age: { type: Number },
   days_key: { type: Number },
   days: [{ type: ObjectId, ref: "Day" }],
