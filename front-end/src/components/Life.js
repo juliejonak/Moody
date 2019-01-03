0f21dd3ad8f13c2803b22e7613f62d3d93911e3a
jordan-massingill <jordan_massingill@hotmail.com>
2019-01-03 12:56:39 -0600

    Updating class to className in JSX on form pages

--- front-end/src/components/Life.js
+++ front-end/src/components/Life.js
@@ -3,22 +3,22 @@ import React from 'react';
 const Life = (props) => {
   return (
     <div>
-        <div class="box box1">
-          <span class="form-text">How'd you eat today?</span>
-          <form> 
-              <span class="eat far fa-thumbs-down" id="eat1"></span>
-              <span class="eat far fa-thumbs-down  fa-rotate-270" id="eat2"> </span>
-              <span class="eat far fa-thumbs-up" id="eat3"></span>
+        <div className="box box1">
+          <span className="form-text">How'd you eat today?</span>
+          <form>
+              <span className="eat far fa-thumbs-down" id="eat1"></span>
+              <span className="eat far fa-thumbs-down  fa-rotate-270" id="eat2"> </span>
+              <span className="eat far fa-thumbs-up" id="eat3"></span>
             </form>
         </div>
 
-        <div class="box box1">
-          <span class="form-text">How did you sleep last night?</span>
-          <span class="form-sub-category">
-            <span class="sleep" id="sleep1">z</span>
-            <span class="sleep" id="sleep2">zZ</span>
-            <span class="sleep" id="sleep3">zZZ</span>
-          </span>      
+        <div className="box box1">
+          <span className="form-text">How did you sleep last night?</span>
+          <span className="form-sub-category">
+            <span className="sleep" id="sleep1">z</span>
+            <span className="sleep" id="sleep2">zZ</span>
+            <span className="sleep" id="sleep3">zZZ</span>
+          </span>
           </div>
     </div>
   )
