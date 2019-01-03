6483047ee45151a5e1d9712c5816d62c744cd57d
Julie Jonak <juliejonak10@gmail.com>
2019-01-03 13:22:52 -0600

    Updated css to show for font awesome design

--- front-end/src/components/Life.js
+++ front-end/src/components/Life.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import '../index.css';
 
 
 const Life = (props) => {
