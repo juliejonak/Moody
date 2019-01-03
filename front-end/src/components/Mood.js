f7efce50de74103f1db3a7d08212a39de74afb14
Julie Jonak <juliejonak10@gmail.com>
2019-01-03 13:15:16 -0600

    Added font awesome icons to Intake and Mood

--- front-end/src/components/Mood.js
+++ front-end/src/components/Mood.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import 'font-awesome/css/font-awesome.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFrown, faSadCry, faMehBlank, faSmile, faGrinBeam } from '@fortawesome/free-solid-svg-icons';
 
 const Mood = (props) => {
   return (
     <div className="box box1">
       <span className="form-text">How do you feel today?</span>
       <form>
-          <span className="mood far fa-sad-cry" id="angry" name="mood" value="1"></span>
-          <span className="mood far fa-frown" id="sad" name="mood" value="2"></span>
-          <span className="mood far fa-meh-blank" id="neutral" name="mood" value="3"></span>
-          <span className="mood far fa-smile" id="happy" name="mood" value="4"></span>
-          <span className="mood far fa-grin-beam" id="thrilled" name="mood" value="5"></span>
+        <FontAwesomeIcon icon={faSadCry} className="mood far" id="angry" name="mood" value="1" />
+        <FontAwesomeIcon icon={faFrown} className="mood far" id="sad" name="mood" value="2" />
+        <FontAwesomeIcon icon={faMehBlank} className="mood far" id="neutral" name="mood" value="3" />
+        <FontAwesomeIcon icon={faSmile} className="mood far" id="happy" name="mood" value="4" />
+        <FontAwesomeIcon icon={faGrinBeam} className="mood far" id="thrilled" name="mood" value="5" />
         </form>
       </div>
       )
