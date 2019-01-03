2d3aff7d185faa57a0722cebf1712590829d2018
jordan-massingill <jordan_massingill@hotmail.com>
2019-01-02 23:19:06 -0600

    working on components and styling

--- front-end/src/components/Mood.js
+++ front-end/src/components/Mood.js
@@ -1,5 +1,14 @@
 export default const Mood = (props) => {
   return (
-
+    <div className="box box1">
+      <span className="form-text">How do you feel today?</span>
+      <form>
+          <span className="mood far fa-sad-cry" id="angry" name="mood" value=1></span>
+          <span className="mood far fa-frown" id="sad" name="mood" value=2></span>
+          <span className="mood far fa-meh-blank" id="neutral" name="mood" value=3></span>
+          <span className="mood far fa-smile" id="happy" name="mood" value=4></span>
+          <span className="mood far fa-grin-beam" id="thrilled" name="mood" value=5><span>
+        </form>
+      </div>
   )
 }
