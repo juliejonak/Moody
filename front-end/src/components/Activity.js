9d8059e216f098e836f59beff6eedd4729077cbe
Julie Jonak <juliejonak10@gmail.com>
2019-01-03 12:30:14 -0600

    Updated carousel components and updateVal method

--- front-end/src/components/Activity.js
+++ front-end/src/components/Activity.js
@@ -1,5 +1,11 @@
-export default const Activity = (props) => {
+import React from 'react';
+
+const Activity = (props) => {
   return (
-    
+    <div>
+      Icons will be here
+    </div>
   )
 }
+
+export default Activity;
--- front-end/src/components/Intake.js
+++ front-end/src/components/Intake.js
@@ -1,5 +1,29 @@
-export default const Intake = (props) => {
-  return (
+import React from 'react';
 
+const Intake = (props) => {
+  return (
+    <div class="box box1">
+      <span class="form-text">What'd you consume?</span>
+      <span class="form-sub-text">Water</span>
+      <span class="form-sub-category">
+          <span class="water fas fa-tint fa-2x" id="water1"></span>
+          <span class="water fas fa-tint fa-3x" id="water2"></span>
+          <span class="water fas fa-tint fa-4x" id="water3"></span>
+      </span>
+      <span class="form-sub-text">Alcohol</span>
+      <span class="form-sub-category">
+          <span class="alcohol fas fa-beer fa-2x" id="alcohol1"></span>
+          <span class="alcohol fas fa-beer fa-3x" id="alcohol2"></span>
+          <span class="alcohol fas fa-beer fa-4x" id="alcohol3"></span>
+      </span>
+      <span class="form-sub-text">Caffeine</span>
+      <span class="form-sub-category">
+          <span class="caffeine fas fa-mug-hot fa-2x" id="caffeine1"></span>
+          <span class="caffeine fas fa-mug-hot fa-3x" id="caffeine2"></span>
+          <span class="caffeine fas fa-mug-hot fa-4x" id="caffeine3"></span>
+      </span>
+    </div>
   )
-}
+};
+
+export default Intake;
