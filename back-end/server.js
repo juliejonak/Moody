d7ed763ada48044ff2a68356238bdb37757f373e
Justin Kaseman <justinkaseman@live.com>
2019-01-03 19:56:15 -0600

    Test in/out for python script

--- back-end/server.js
+++ back-end/server.js
@@ -1,3 +1,4 @@
+let { PythonShell } = require("python-shell");
 const express = require("express");
 const helmet = require("helmet");
 
@@ -5,7 +6,7 @@ const db = require("./db.js");
 const usersRouter = require("./users/usersRouter.js");
 const daysRouter = require("./days/daysRouter.js");
 
-const cors = require('cors');
+const cors = require("cors");
 
 const server = express();
 
@@ -21,6 +22,59 @@ server.use("/api/users", usersRouter);
 server.use("/api/days", daysRouter);
 
 server.get("/", (req, res) => {
+  let pyshell = new PythonShell("test.py");
+  const data = JSON.stringify({
+    days: [
+      {
+        tags: ["Travel", "Work"],
+        _id: "5c2da9cc3f6bfcb9fc9b320a",
+        user: "5c2da1cf3f6bfcb9fc9b31ff",
+        happiness: 1,
+        stress: 2,
+        energy: 3,
+        sleep: 2,
+        water: 2,
+        alcohol: 2,
+        caffiene: 2,
+        food: 2,
+        date: "2019-01-03T06:21:00.866Z",
+        __v: 0,
+      },
+      {
+        tags: ["Travel", "Work"],
+        _id: "5c2da9ff3f6bfcb9fc9b320b",
+        user: "5c2da9413f6bfcb9fc9b3208",
+        happiness: 1,
+        stress: 2,
+        energy: 3,
+        sleep: 2,
+        water: 2,
+        alcohol: 2,
+        caffiene: 2,
+        food: 2,
+        date: "2019-01-03T06:21:51.417Z",
+        __v: 1,
+      },
+    ],
+    _id: "5c2da9413f6bfcb9fc9b3208",
+    name: "abc",
+    gender: "M",
+    age: 12,
+    __v: 0,
+  });
+  // sends a message to the Python script via stdin
+  pyshell.send(data);
+
+  pyshell.on("message", function(message) {
+    // received a message sent from the Python script (a simple "print" statement)
+    console.log(message);
+  });
+
+  // end the input stream and allow the process to exit
+  pyshell.end(function(err, code, signal) {
+    if (err) throw err;
+    console.log("finished");
+  });
   res.send("Run API run");
 });
 
