fcaab35cec598c2f336b92fc7d8e205c9ad34271
Julie Jonak <juliejonak10@gmail.com>
2019-03-17 21:23:45 -0500

    Added in heroku deploy link to FE

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -24,7 +24,7 @@ export default class App extends Component {
 
   componentDidMount = () => {
     axios
-      .get("http://localhost:5000/api/days")
+      .get("https://moodyio.herokuapp.com/api/days")
       .then(response => {
         console.log("data success", response);
       })
